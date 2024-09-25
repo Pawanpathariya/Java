7bd93092c47d52be3923048c7dbf422cdb3eb390
Pawanpathariya <pawanpathariys@gmail.com>
2024-09-25 21:56:16 +0530

    addition

---
+++ Sum.java
@@ -0,0 +1,13 @@
+import  java.util.*;
+public class sum {
+    public static void main(String[] args) {
+      Scanner s=new Scanner(System.in);
+      System.out.println("Enter number "); 
+      int a=s.nextInt();
+      System.out.println("Enter number "); 
+      int b=s.nextInt();
+      int c= a+b;
+      System.out.println("total = "+c); 
+    }
+}
+
